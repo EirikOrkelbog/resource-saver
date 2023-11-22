05761944fd41ec02cb970f29610e5e8c0b89b54c
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-11-22 21:15:10 +0100

    added some sample resources and looped through them

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li v-for="res in storedResources" :key="res.id">
+      <div>
+        <header>
+          <h3>{{ res.title }}</h3>
+          <button>Delete</button>
+        </header>
+      </div>
+      <p>{{ res.description }}</p>
+      <nav>
+        <a :href="res.link">See resource</a>
+      </nav>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official guide',
+          title: 'Official guide',
+          description: 'Official Vue guide',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Google website',
+          link: 'https://www.google.com/',
+        }
+      ],
+    }
+  }
+}
+</script>
